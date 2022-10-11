c8b8d491ccef4d7aaf27e37bd9a33c22d28270e2
Nguyen Phung Vinh <vinhnguyen69dn@gmail.com>
2022-10-11 21:06:44 +0700

    cap nhat 11/10

---
+++ src/api/orderApi.js
@@ -0,0 +1,11 @@
+import axiosClient from "./axiosClient";
+// api/orderApi.js
+class OrderApi {
+    getAll = (params) => {
+        const url = '/orders';
+        return axiosClient.get(url, { params });
+    };
+}
+const orderApi = new OrderApi();
+
+export default orderApi;
