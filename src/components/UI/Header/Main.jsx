ea8a554643008de0d199708d06d14036fcb8bfc4
Nguyen Phung Vinh <vinhnguyen69dn@gmail.com>
2022-10-20 21:36:27 +0700

    Update code

--- src/components/UI/Header/Main.jsx
+++ src/components/UI/Header/Main.jsx
@@ -3,7 +3,10 @@ import React,{ PureComponent } from "react";
 class UIHeaderMain extends PureComponent{
     render(){
         return(
-            <div>Main Header chứa: Logo, Search, giỏ hàng, cài đặt,.v.v</div>
+            <div>
+                Main Header chứa: Logo, Search, giỏ hàng, cài đặt,.v.v
+                </div>
+            
         )
     }
 }
