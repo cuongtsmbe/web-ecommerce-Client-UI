5f971e633b4ae78ed6bb94fa3790092951adb6a5
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-10-20 23:25:16 +0700

    Updates

---
+++ precache-manifest.2f5a020a09d8e9f1389ebac815e13cab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d2a5f518b948d4f903a1",
+    "url": "/PTPMNMN/static/css/main.d64b9696.chunk.css"
+  },
+  {
+    "revision": "d2a5f518b948d4f903a1",
+    "url": "/PTPMNMN/static/js/main.d2a5f518.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/PTPMNMN/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "8e4fabfa79a8eded5a3c",
+    "url": "/PTPMNMN/static/js/2.8e4fabfa.chunk.js"
+  },
+  {
+    "revision": "68e005796285ecfb572b",
+    "url": "/PTPMNMN/static/js/runtime~main.68e00579.js"
+  },
+  {
+    "revision": "292f89f9617c368c09cd640b2687affd",
+    "url": "/PTPMNMN/static/media/users-cog-solid.292f89f9.svg"
+  },
+  {
+    "revision": "57105e6436cc024bf72412eff9e70e56",
+    "url": "/PTPMNMN/index.html"
+  }
+];
