5dc5dfb5342f04977a4392de940fbcb245a6b9cb
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-10-24 22:59:40 +0700

    Bau 24/10

--- src/components/UI/Header/Navigation.jsx
+++ src/components/UI/Header/Navigation.jsx
@@ -7,27 +7,11 @@ class UIHeaderNavigation extends PureComponent{
 		categories: []
 	}
 	async componentDidMount() {
-		try {
-			const response = await categoryApi.getAll();
-			const categories = response.data;
-			this.setState({ categories });			
-		} catch (error) {
-			console.log('Failed to fetch category list:', error);
-		}
+		
 	}
 	render() {
 		return (
-			<nav id="navigation">
-				<div className="container">
-					<div id="responsive-nav">
-						<ul className="main-nav nav navbar-nav">
-							<li><Link to='/'>Trang Chủ</Link></li>
-							<li><Link to='/categories'>Tùy Chọn</Link></li>
-							{this.state.categories.map((category,index )=> <li key={index}><Link  to={`/categories/${category.id}`}>{category.ten_the_loai}</Link></li>)}
-						</ul>
-					</div>
-				</div>
-			</nav>
+			<div>Thanh dieu huong</div>
 		)
 	}
 }
--- src/components/UI/Header/Top.jsx
+++ src/components/UI/Header/Top.jsx
@@ -4,26 +4,7 @@ import { Link } from 'react-router-dom';
 class UIHeaderTop extends PureComponent{
     render(){
         return(
-            <div id="top-header">
-            <div className="container">
-                <ul className="header-links pull-left">
-                    <li><Link><i className="fa fa-phone"></i> 0987654321</Link></li>
-                    <li><Link><i className="fa fa-envelope-o"></i> gnine@nineg.com.vn</Link></li>
-                    <li><Link><i className="fa fa-map-marker"></i> ABC, Việt Nam</Link></li>
-                </ul>
-                <ul className="header-links pull-right">
-                    <li><Link><i className="fa fa-dollar"></i> VNĐ</Link></li>
-                    <li><Link><i className="fa fa-user-o"></i> Tạo tài khoản</Link></li>
-                    {/* <?php
-                    if(isset($_SESSION['ten_dangnhap'])){
-                        $ten_dangnhap = $_SESSION['ten_dangnhap'];
-                    echo '<li><a href="?act=my_account"><i className="fa fa-user-o"></i> Xin chào, '.$ten_dangnhap.'!</a></li>';
-                    }
-                    else echo '<li><a href="index.php?act=register"><i className="fa fa-user-o"></i> Tạo tài khoản</a></li>';
-                ?> */}
-                </ul>
-            </div>
-        </div>
+           <div>Top HEader</div>
         )
     }
 }
