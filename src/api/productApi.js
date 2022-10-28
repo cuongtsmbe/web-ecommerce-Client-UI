446b6d71b5efdd28b0b87bbd3db1a3bd31602baa
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-10-28 11:33:28 +0700

    edit handle call api

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -1,16 +1,17 @@
 import axiosClient from "./axiosClient";
+import LINK from "../utils/link.json";
 // api/productApi.js
 class ProductApi {
     getAll = (params) => {
-        const url = '/product/list';
+        const url = LINK.CLIENT.PRODUCT_GET_LIST;
         return axiosClient.get(url, { params });
     };
     getDetailProductById = (id) => {
-        const url = `/product/details/${id}`;
+        const url = `${LINK.CLIENT.PRODUCT_GET_DETAILS}/${id}`;
         return axiosClient.get(url);
     };
     getTop10Saler = (params) => {
-        const url = `/product/topsale/10`;
+        const url = `${LINK.CLIENT.PRODUCT_GET_LIST_TOPSALE_MOST}/10`;
         return axiosClient.get(url, { params });
     }
 }
