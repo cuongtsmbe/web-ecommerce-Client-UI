8b8961c3282ca6beac17c0753548655651a788b1
ngocbau12324 <ngocbau2015tqk@gmail.com>
2022-10-28 15:57:47 +0700

    No comment

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -11,15 +11,11 @@ class ProductApi {
         return axiosClient.get(url);
     };
     getTop10Saler = (params) => {
-<<<<<<< HEAD
-        const url = `/product/topsale/10`;
-=======
         const url = `${LINK.CLIENT.PRODUCT_GET_LIST_TOPSALE_MOST}/10`;
         return axiosClient.get(url, { params });
     }
     getProductRelevant = (idCategory, idProduct, params) => {
         const url = `${LINK.CLIENT.PRODUCT_GET_LIST_RELEVANT}/${idCategory}/${idProduct}/4`;
->>>>>>> 50ffe0e8eba3eec655f3b76333ce54a1de50d13d
         return axiosClient.get(url, { params });
     }
 }
